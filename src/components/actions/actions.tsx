72f7dca8b80f66392b7cbbe03038e991cc873a5a
Gleb <basketking57@gmail.com>
2023-01-20 14:43:51 +0200

    Update actions.tsx

--- src/components/actions/actions.tsx
+++ src/components/actions/actions.tsx
@@ -2,10 +2,10 @@ import { createAction } from "@reduxjs/toolkit";
 
 export const currentWeatherFetching = createAction('CURRENT_WEATHER_FETCHING');
 export const currentWeatherFetched = createAction('CURRENT_WEATHER_FETCHED');
-export const currentWeatherFetchedError = createAction('CURRENT_WEATHER_FETCHED_ERROR');
+export const currentWeatherFetchedError = createAction('CURRENT_WEATHER_FETCHING_ERROR');
 
 
 
 export const forecastFetching = createAction('FORECAST_FETCHING');
 export const forecastFetched = createAction('FORECAST_FETCHED');
-export const forecastFetchedError = createAction('FORECAST_FETCHED_ERROR');
+export const forecastFetchedError = createAction('FORECAST_FETCHING_ERROR');
