20983720f003b14275fccb147e8a47d1405d2fb0
Gleb <basketking57@gmail.com>
2023-01-20 13:28:59 +0200

    fix

--- src/components/actions/actions.tsx
+++ src/components/actions/actions.tsx
@@ -3,7 +3,7 @@ import { createAction } from "@reduxjs/toolkit";
 export const apiFetching = createAction('API_FETCHING');
 
 export const apiFetched = createAction('API_FETCHED');
-
+    
 export const apiFetchingError = createAction('API_FETCHING_ERROR');
 
 
--- src/components/services/WeatherServices.tsx
+++ src/components/services/WeatherServices.tsx
@@ -2,7 +2,7 @@ import { useHttp } from "../hooks/http.hook";
 
 const useWeatherServices = () => {
 
-    const _apiBase: string = 'http://api.weatherapi.com/v1';
-    const _apiKey: string = '8e5f9c7c74674207b8813736231401';
+    const _apiBase: string = 'http://api.weatherapi.com/v1/current.json?';
+    const _apiKey: string = 'key=8e5f9c7c74674207b8813736231401';
     
 }
