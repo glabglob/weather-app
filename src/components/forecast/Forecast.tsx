cb5d23bbb659d594ad90403fadbcf51848ce0964
Gleb <basketking57@gmail.com>
2023-01-20 13:58:01 +0200

    Update Forecast.tsx

--- src/components/forecast/Forecast.tsx
+++ src/components/forecast/Forecast.tsx
@@ -4,13 +4,13 @@ const Forecast: React.FC = () => {
     return (
         <ul className="forecast__list">
             <h2>Forecast for 7 days</h2>
-            <li className="forecast">15</li>
-            <li className="forecast">22</li>
-            <li className="forecast">11</li>
-            <li className="forecast">24</li>
-            <li className="forecast">24</li>
-            <li className="forecast">24</li>
-            <li className="forecast">24</li>
+            <li className="forecast">15°</li>
+            <li className="forecast">22°</li>
+            <li className="forecast">11°</li>
+            <li className="forecast">24°</li>
+            <li className="forecast">24°</li>
+            <li className="forecast">24°</li>
+            <li className="forecast">24°</li>
         </ul>
     );
 }
