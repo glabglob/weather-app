6cbfbd01e63389f5f3aaba527745e9cbd7de66b9
Gleb <basketking57@gmail.com>
2023-01-29 22:34:01 +0200

    Update weatherApiServices.tsx

--- src/components/services/weatherApiServices.tsx
+++ src/components/services/weatherApiServices.tsx
@@ -3,7 +3,7 @@
 // http://api.weatherapi.com/v1/search.json?key=8e5f9c7c74674207b8813736231401&q=Paris
 
 // API PARTS
-export const _apiBase: string = 'http://api.weatherapi.com/v1/current.json?';
+export const _apiBase: string = 'https://api.weatherapi.com/v1/current.json?';
 export const _apiKey: string = 'key=8e5f9c7c74674207b8813736231401';
 export const _apiParams: string = '&aqi=no&alerts=no';
 
